127f0585bdd09c3483dccc661a56b79905b2b2ae
nqp2k3 <90236730+NQP27@users.noreply.github.com>
2023-04-08 17:08:47 +0700

    Add files via upload

---
+++ danh_sach_canh_to_ma_tran_ke.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define N 10000
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int mt[100][100] = {0};
+        int n, m;
+        cin >> n >> m;
+        for (int i = 1; i <= m; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            mt[a][b] = 1;
+            mt[b][a] = 1;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                cout << mt[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
